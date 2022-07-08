1a0550d31751ed059a576f474d05bd546ffc37bb
Houjun Liu <hliu@shabang.cf>
2022-07-07 21:31:10 -0700

    adding check results

--- check.py
+++ check.py
@@ -20,67 +20,77 @@ repath_file = lambda file_path, new_dir: os.path.join(new_dir, pathlib.Path(file
 CLAN_PATH=""
 
 # file to check
-CHECKFILE="/Users/houliu/Documents/Projects/talkbank-alignment/EllisWeismer/30ec/TD/v2/out/11012.cha"
+CHECKDIR="/Users/houliu/Documents/Projects/talkbank-alignment/EllisWeismer/30ec/TD/v2/out/"
 
-# run flo on the file
-CMD = f"{os.path.join(CLAN_PATH, 'flo +t%xwor')} {CHECKFILE} >/dev/null 2>&1"
-# run!
-os.system(CMD)
+# get output
+files = globase(CHECKDIR, "*.cha")
 
-# path to result
-result_path = CHECKFILE.replace("cha", "flo.cex")
+# for each file
+for checkfile in files:
 
-# read in the resulting file
-with open(result_path, "r") as df:
-    # get alignment result
-    data = df.readlines()
+    # run flo on the file
+    CMD = f"{os.path.join(CLAN_PATH, 'flo +t%xwor')} {checkfile} >/dev/null 2>&1"
+    # run!
+    os.system(CMD)
 
-# conform result with tab-seperated beginnings
-result = []
-# for each value
-for value in data:
-    # if the value continues from the last line
-    if value[0] == "\t":
-        # pop the result
-        res = result.pop()
-        # append
-        res = res.strip("\n") + value[1:]
-        # put back
-        result.append(res)
-    else:
-        # just append typical value
-        result.append(value)
+    # path to result
+    result_path = checkfile.replace("cha", "flo.cex")
 
-# new the result
-result = [re.sub("\\x15.*?\\x15", "", i) for i in result] # bullets
-result = [re.sub(".*?\\t", "", i) for i in result] # tabs
-result = [re.sub("\.", "", i) for i in result] # doduble spaces
-result = [re.sub("  ", " ", i).strip() for i in result] # doduble spaces
+    # read in the resulting file
+    with open(result_path, "r") as df:
+        # get alignment result
+        data = df.readlines()
 
-# get paired results
-result_paired = []
+    # conform result with tab-seperated beginnings
+    result = []
+    # for each value
+    for value in data:
+        # if the value continues from the last line
+        if value[0] == "\t":
+            # pop the result
+            res = result.pop()
+            # append
+            res = res.strip("\n") + value[1:]
+            # put back
+            result.append(res)
+        else:
+            # just append typical value
+            result.append(value)
 
-# pair up results
-for i in range(0, len(result)-3, 3):
-    # append paired result
-    result_paired.append((result[i], result[i+2]))
+    # new the result
+    result = [re.sub(" \\x15\d*_\d*\\x15>", ">", i) for i in result] # bullets
+    result = [re.sub("\\x15.*?\\x15", "", i) for i in result] # bullets
+    result = [re.sub(".*?\\t", "", i) for i in result] # tabs
+    result = [re.sub("\.", "", i) for i in result] # doduble spaces
+    result = [re.sub("  ", " ", i).strip() for i in result] # doduble spaces
 
-# check for equivalence and report error
-errors = []
+    # get paired results
+    result_paired = []
 
-# tabulate errors
-for indx,(i,j) in enumerate(result_paired):
-    if i != j:
-        errors.append(indx)
+    # pair up results
+    for i in range(0, len(result)-3, 3):
+        # append paired result
+        result_paired.append((result[i], result[i+2]))
 
-# print results
-print()
-print(f"check.py output on {os.path.basename(CHECKFILE)}")
-print("-----------------------------\n")
+    # check for equivalence and report error
+    errors = []
 
-# for the error
-for item in errors:
-    print(f"Main tier: {result_paired[item][0]}")
-    print(f"%xwor tier: {result_paired[item][1]}\n")
+    # tabulate errors
+    for indx,(i,j) in enumerate(result_paired):
+        if i != j:
+            errors.append(indx)
+
+    # print results
+    print()
+    print(f"check.py output on {os.path.basename(checkfile)}")
+    print("-----------------------------\n")
+
+    # for the error
+    for item in errors:
+        print(f"Main tier: {result_paired[item][0]}")
+        print(f"%xwor tier: {result_paired[item][1]}\n")
+    # if no errors, print
+    if len(errors) == 0:
+        print("No inconsistencies found!\n")
 
 
