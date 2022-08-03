81f75beb6353a7264854e4a26e7b2d2282f409c7
Houjun Liu <hliu@shabang.cf>
2022-08-02 21:35:02 -0400

    thanks.

--- setup/darwin.sh
+++ setup/darwin.sh
@@ -9,3 +9,21 @@ tput setaf 2
 echo "Done."
 tput sgr0
 
+# Space question
+echo -e "\nWe are setting up batchalign for your system."
+echo "This will consist the installation of a few"
+echo "tools, which take about 5GB of space. Please"
+echo "ensure that space is available. Please press"
+read -rsn1 -p"enter to continue: "
+
+tput setaf 2
+echo "thanks."
+tput sgr0
+
+# create newline
+echo
+
+
+
+
+
