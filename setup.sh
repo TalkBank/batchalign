199da521d17a80462ad31cc4c04077e797a37ddc
Houjun Liu <hliu@shabang.cf>
2022-08-02 21:24:41 -0400

    new setup welcome

--- setup.sh
+++ setup.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
+# Welcome the user
+tput bold 
+echo -e "\nWelcome to batchalign setup!"
+tput sgr0 
+
+# Print the environment
+echo "We have detected your operating system as $OSTYPE." 
+
 # detect operating system, and trigger the right command
 case $OSTYPE in
     *darwin*)
+        echo -n "Pulling and running the darwin setup script! " 
         /usr/bin/env bash -c "$(curl -fsSL https://raw.githubusercontent.com/TalkBank/batchalign/master/setup/darwin.sh)"
 esac
--- setup/darwin.sh
+++ setup/darwin.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
-echo test
+# Print the "done" as the new setup has taken over
+# This file is pulled with "curl" by the welcome script; this "done"
+# just tells the user that the new script has sucessfully been
+# downloaded.
+
+tput setaf 2
+echo "Done!"
+tput sgr0
+
+
 
 
