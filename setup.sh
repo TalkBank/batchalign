b5abe6319aec0d5098ccb3ffd6b53c3ac59b4014
Houjun Liu <hliu@shabang.cf>
2022-08-02 21:15:03 -0400

    updating setup files

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # detect operating system, and trigger the right command
-case $OSTYPE
-
-
+case $OSTYPE in
+    *darwin*)
+        /usr/bin/env bash -c "$(curl -fsSL https://raw.githubusercontent.com/TalkBank/batchalign/master/setup/darwin.sh)"
+esac
