c5c70610ca66f93e236cf917fef53bbc64573782
Houjun Liu <hliu@shabang.cf>
2022-08-03 10:04:25 -0400

    updating config

--- setup/execute.sh
+++ setup/execute.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# clear screen
+# clear
+
 # print done
 isdone () {
     tput setaf 2
@@ -18,15 +21,30 @@ prompt() {
     tput sgr0
 }
 
-# Prompt for mode
-tput bold
-echo "#### Run Configuration ####"
-tput sgr0
+# # prompt for info
+# tput bold
+# echo "Welcome to batchalign"
+# tput sgr0
+# echo "Let's configure your current run."
+# echo
 
-# choose option
-MODE=$(gum choose "Analyze Audio" "Align CHAT")
+# # Prompt for mode
+# tput bold
+# echo "#### Run Configuration ####"
+# tput sgr0
 
+# # choose main mode option
+# MODE=$(gum choose "Analyze Raw Audio" "Analyze Rev.AI Output" "Realign CHAT")
+# echo -n "Our current run will $(tput setaf 2)$MODE"
+# tput sgr0
+# echo "."
 
-# # run!
-# docker-compose up --build
+# case $MODE in
+#     "Analyze Raw Audio")
+#         echo "test"
+#         ;;
+# esac
+
+# run!
+docker-compose up --build
 
