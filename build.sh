b16caa32f69e908cdd6f352c2149bdc1937cb4f3
Houjun Liu <houjun@jemoka.com>
2022-09-29 23:26:49 -0700

    updating build to use generic complier

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 pip install .
 make -j8 -C ./baln/opt/CLANUtils/src/ -f "makefile.$(UNAME)"
 mkdir -p $PREFIX/bin
-ls ./baln/opt/CLANUtils/unix/bin/
 mv ./baln/opt/CLANUtils/unix/bin/* $PREFIX/bin
 chmod u+x $PREFIX/bin/*
+
