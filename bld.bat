a6814327a94f86d7e4ee393a1ac2a448e6ce6005
Houjun Liu <houjun@jemoka.com>
2022-10-21 10:15:54 -0700

    moving file

--- bld.bat
+++ bld.bat
@@ -1,5 +1,5 @@
 pip install .
-make -j8 -C ./baln/opt/CLANUtils/src/ -f "Makefile.win"
+make -j8 -C ./baln/opt/CLANUtils/src/
 mkdir -p $PREFIX/bin
 mv ./baln/opt/CLANUtils/unix/bin/* $PREFIX/bin
 chmod u+x $PREFIX/bin/*
