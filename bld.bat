fe3ceee99752380102275d0c3d30868e7e1f9f1c
Houjun Liu <houjun@jemoka.com>
2022-10-24 00:03:16 -0700

    installs both

--- bld.bat
+++ bld.bat
@@ -1,7 +1,7 @@
 pip install .
 cd ./baln/opt/CLANUtils/unix
-cmake ../src
+cmake -G "MinGW Makefiles" ../src
 make 
 mkdir -p %PREFIX%/bin
-mv ./bin/* $PREFIX/bin
-chmod u+x $PREFIX/bin/*
+mv ./bin/* %PREFIX%/bin
+chmod u+x %PREFIX%/bin/*
