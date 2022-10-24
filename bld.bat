c24fbf8dfc0513788dc4bc8ae3fce174e135dea4
Houjun Liu <houjun@jemoka.com>
2022-10-23 19:34:58 -0700

    removes decorator

--- bld.bat
+++ bld.bat
@@ -1,7 +1,6 @@
 pip install .
 make -d -C ./baln/opt/CLANUtils/src/ -f Makefile.Windows
-dir $PREFIX/bin
-rem mkdir -p $PREFIX/bin
+mkdir -p %PREFIX%/bin
 rem mv ./baln/opt/CLANUtils/unix/bin/* $PREFIX/bin
 rem chmod u+x $PREFIX/bin/*
 
