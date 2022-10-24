6fa35e20f60f3053ffd4f68e96013181937dd140
Houjun Liu <houjun@jemoka.com>
2022-10-23 18:48:18 -0700

    explicit windows makefile

--- bld.bat
+++ bld.bat
@@ -1,5 +1,5 @@
 pip install .
-make -d -C ./baln/opt/CLANUtils/src/
+make -d -C ./baln/opt/CLANUtils/src/ -f Makefile.Windows
 dir $PREFIX/bin
 rem mkdir -p $PREFIX/bin
 rem mv ./baln/opt/CLANUtils/unix/bin/* $PREFIX/bin
