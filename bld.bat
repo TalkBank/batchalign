7f73c21d1c7c7aaccb7118a3a09f3c568602fae0
Houjun Liu <houjun@jemoka.com>
2022-10-23 22:46:29 -0700

    verbose make

--- bld.bat
+++ bld.bat
@@ -1,5 +1,5 @@
 pip install .
-make -C ./baln/opt/CLANUtils/src/ -f Makefile.Windows
+make VERBOSE=1 -C ./baln/opt/CLANUtils/src/ -f Makefile.Windows
 mkdir -p %PREFIX%/bin
 rem mv ./baln/opt/CLANUtils/unix/bin/* $PREFIX/bin
 rem chmod u+x $PREFIX/bin/*
