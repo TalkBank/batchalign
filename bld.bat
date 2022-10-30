46fe7766ab720d990773f23ee4fc5844c018c020
Houjun Liu <houjun@jemoka.com>
2022-10-29 20:29:58 -0700

    removed inline make command

--- bld.bat
+++ bld.bat
@@ -1,11 +1,7 @@
 @echo on
-echo step 1
 pip install .
-echo step 2
 cd baln\opt\CLANUtils\unix
-echo step 3
-make
-echo step 4
+REM make
 mkdir -p %PREFIX%\bin
 mv bin\* %PREFIX%\bin
 chmod u+x %PREFIX%\bin\*
