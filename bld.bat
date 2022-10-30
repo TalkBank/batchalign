480cd7b78bb80cb24443b4b993b5b772c66b4540
Houjun Liu <houjun@jemoka.com>
2022-10-29 20:05:50 -0700

    updated cd command order

--- bld.bat
+++ bld.bat
@@ -1,6 +1,6 @@
 pip install .
-cd ./baln/opt/CLANUtils/unix
+cd baln\opt\CLANUtils\unix
 make
-mkdir -p %PREFIX%/bin
-mv ./bin/* %PREFIX%/bin
-chmod u+x %PREFIX%/bin/*
+mkdir -p %PREFIX%\bin
+mv .\bin\* %PREFIX%\bin
+chmod u+x %PREFIX%\bin\*
