dfb9de0bca2d8dbbda0ff57612b16f6800f3c77d
Houjun Liu <houjun@jemoka.com>
2022-11-21 23:01:01 -0800

    moving elan utilities to another file

--- baln/command_line.py
+++ baln/command_line.py
@@ -30,22 +30,36 @@ def cli():
         MODE = 0
 
     # manloop to take input
-    parser = argparse.ArgumentParser(description="batch align .cha to audio in a directory with MFA")
+    parser = argparse.ArgumentParser(description="CHAT transcript batch-processing utilities")
+
+    # seperate commands
+    subparsers = parser.add_subparsers(dest="command")
+
+    #### subparsers ####
+    # .wav => .cha full generation commands
+    generate = subparsers.add_parser("generate")
+    generate.add_argument('-i', "--interactive", default=False, action='store_true', help='interactive retokenization (with user correction), useless without retokenize')
+    generate.add_argument('-n', "--headless", default=False, action='store_true', help='interactive without GUI prompt, useless without -i')
+    generate.add_argument('-a', "--asronly", default=False, action='store_true', help='ASR only, don\'t run mfa')
+    generate.add_argument("--utterancemodel", type=str, help='path to utterance tokenization model')
+    generate.add_argument("--rev", type=str, help='rev.ai API key, to submit audio')
+
+    # .wav + .cha => .cha %wor alignment commands
+    alignment = subparsers.add_parser("align")
+    alignment.add_argument("--beam", type=int, default=30, help='beam width for MFA, ignored for P2FA')
+    alignment.add_argument("--skipalign", default=False, action='store_true', help='don\'t align, just call CHAT ops')
+    alignment.add_argument("--prealigned", default=False, action='store_true', help='input .cha has utterance-level alignments')
+
+    # cleanup commands
+    clean = subparsers.add_parser("clean")
+
+    #### master commands ####
     parser.add_argument("in_dir", type=str, help='input directory containing .cha and .mp3/.wav files')
     parser.add_argument("out_dir", type=str, help='output directory to store aligned .cha files')
-    parser.add_argument("--prealigned", default=False, action='store_true', help='input .cha has utterance-level alignments')
     parser.add_argument("--data_dir", type=str, default="data", help='subdirectory of out_dir to use as data dir')
-    parser.add_argument("--beam", type=int, default=30, help='beam width for MFA, ignored for P2FA')
-    parser.add_argument("--skipalign", default=False, action='store_true', help='don\'t align, just call CHAT ops')
     parser.add_argument("--skipclean", default=False, action='store_true', help='don\'t clean')
     parser.add_argument("--dictionary", type=str, help='path to custom dictionary')
-    parser.add_argument("--model", type=str, help='path to custom model')
-    parser.add_argument("--retokenize", type=str, help='retokenize input with model')
-    parser.add_argument('-i', "--interactive", default=False, action='store_true', help='interactive retokenization (with user correction), useless without retokenize')
-    parser.add_argument('-n', "--headless", default=False, action='store_true', help='interactive without GUI prompt, useless without -i')
-    parser.add_argument('-a', "--asronly", default=False, action='store_true', help='ASR only, don\'t run mfa')
-    parser.add_argument("--rev", type=str, help='rev.ai API key, to submit audio')
-    parser.add_argument("--clean", default=False, action='store_true', help='don\'t align, just call cleanup')
+    parser.add_argument("--alignmentmodel", type=str, help='path to custom kaldi alignmetn model')
 
     return MODE, REV_API, parser.parse_args()
 
@@ -65,19 +79,18 @@ def mainloop():
     MODE, REV_API, args = cli()
 
     # if we are cleaning
-    if args.clean:
+    if args.command =="clean":
+        print("Stage 1: Cleaning Folders")
         cleanup(args.in_dir, args.out_dir, args.data_dir)
+        print("All done! Check the input folder.")
         # if we need to retokenize or run with audio only (i.e. no files avaliable)
-    elif (args.retokenize and MODE != 0) or ((len(globase(args.in_dir, "*.cha")) == 0) and
-                                             (len(globase(args.in_dir, "*.json")) == 0)) or MODE == 1:
-        # assert retokenize
-        # assert args.retokenize, "Only audio files provided, but no segmentation model provided with --retokenize!"
+    elif args.command =="generate":
         # assert retokenize
         print("Stage 1: Performing ASR")
-        retokenize_directory(args.in_dir, args.retokenize if args.retokenize else "~/mfa_data/model", 'h' if args.headless else args.interactive, args.rev)
+        retokenize_directory(args.in_dir, args.utterancemodel if args.utterancemodel else "~/mfa_data/model", 'h' if args.headless else args.interactive, args.rev)
         if not args.asronly:
             print("Stage 2: Performing Forced Alignment")
-            do_align(args.in_dir, args.out_dir, args.data_dir, prealigned=True, beam=args.beam, align=(not args.skipalign), clean=(not args.skipclean), dictionary=args.dictionary, model=args.model)
+            do_align(args.in_dir, args.out_dir, args.data_dir, prealigned=True, beam=args.beam, align=(not args.skipalign), clean=(not args.skipclean), dictionary=args.dictionary, model=args.alignmentmodel)
         else:
             # Define the data_dir
             DATA_DIR = os.path.join(args.out_dir, args.data_dir)
@@ -88,9 +101,10 @@ def mainloop():
 
             # cleanup
             cleanup(args.in_dir, args.out_dir, args.data_dir)
-            print("All done! Check the input folder.")
-            # otherwise prealign
-    else: 
-        print("Stage 1: Performing Forced Alignment")
-        do_align(args.in_dir, args.out_dir, args.data_dir, prealigned=(True if MODE==0 else args.prealigned), beam=args.beam, align=(not args.skipalign), clean=(not args.skipclean), dictionary=args.dictionary, model=args.model)
         print("All done! Check the input folder.")
+    elif args.command == "align": 
+        print("Stage 1: Performing Forced Alignment")
+        do_align(args.in_dir, args.out_dir, args.data_dir, prealigned=(True if MODE==0 else args.prealigned), beam=args.beam, align=(not args.skipalign), clean=(not args.skipclean), dictionary=args.dictionary, model=args.alignmentmodel)
+        print("All done! Check the input folder.")
+    else:
+        raise Exception("Unknown command passed to parser!")
---
+++ baln/eaf.py
@@ -0,0 +1,318 @@
+# XML facilities
+import xml.etree.ElementTree as ET
+
+# Also, stdlib
+import re, os
+
+# glob
+import glob
+
+# import cleanup
+from .utils import resolve_clan
+
+CLAN_PATH=resolve_clan()
+
+# Oneliner of directory-based glob and replace
+globase = lambda path, statement: glob.glob(os.path.join(path, statement))
+repath_file = lambda file_path, new_dir: os.path.join(new_dir, pathlib.Path(file_path).name)
+bullet = lambda start,end: f" {int(round(start*1000))}_{int(round(end*1000))}" # start and end should be float seconds
+
+# indent an XML
+def indent(elem, level=0):
+    """Indent an XML
+
+    Attributes:
+        element (ET.ElementTree.root): element tree root
+        level (int): which level to start aligning from
+
+    Citation:
+    https://stackoverflow.com/a/33956544/5684313
+
+    Returns void
+    """
+
+    i = "\n" + level*"  "
+    if len(elem):
+        if not elem.text or not elem.text.strip():
+            elem.text = i + "  "
+        if not elem.tail or not elem.tail.strip():
+            elem.tail = i
+        for elem in elem:
+            indent(elem, level+1)
+        if not elem.tail or not elem.tail.strip():
+            elem.tail = i
+    else:
+        if level and (not elem.tail or not elem.tail.strip()):
+            elem.tail = i
+
+def eafalign(file_path, alignments, output_path):
+    """get an unaligned eaf file to be aligned
+
+    Attributes:
+        file_path (string): file path of the .eaf file
+        alignments (list or dict): output of transcript word alignment
+                                   long or short form will generate diff. results
+        output_path (string): output path of the aligned .eaf file
+
+    Returns:
+        None
+    """
+
+    # Load the file
+    parser = ET.XMLParser(encoding="utf-8")
+    tree = ET.parse(file_path, parser=parser)
+    root =  tree.getroot()
+    tiers = root[2:]
+
+    # create an array for annotations
+    annotations = []
+
+    # For each tier 
+    for tier in tiers:
+        # get the name of the tier
+        tier_id = tier.attrib.get("TIER_ID","")
+
+        # we ignore anything that's a "@S*" tier
+        # because those are metadata
+        if "@" in tier_id:
+            continue
+
+        # For each annotation
+        for annotation in tier:
+
+            # Parse timeslot ID
+            try: 
+                timeslot_id_1 = int(annotation[0].attrib.get("TIME_SLOT_REF1", "0")[2:])
+                timeslot_id_2 = int(annotation[0].attrib.get("TIME_SLOT_REF2", "0")[2:])
+            except ValueError:
+                print(file_path)
+
+            # and append the metadata + transcript to the annotations
+            annotations.append(((timeslot_id_1,timeslot_id_2),
+                                tier_id,
+                                annotation[0].attrib.get("ANNOTATION_ID", "0")))
+
+    # we will sort annotations by timeslot ref
+    annotations.sort(key=lambda x:int(x[-1][1:]))
+
+    # if we are doing long-form alignment
+    if type(alignments) == dict:
+        # create a lookup dict 
+        terms_flattened = list(zip([i[-1] for i in annotations], alignments["terms"]))
+        terms_dict = dict(terms_flattened)
+
+        # set alignment back to what the rest of the function would expect
+        alignments = alignments["alignments"]
+
+        # Create the xword annotation ID index
+        id_indx = 0
+
+        # replace content with the bulleted version
+        # For each tier 
+        for tier in tiers:
+            # get the name of the tier
+            tier_id = tier.attrib.get("TIER_ID","")
+
+            # create new xword tier
+            xwor_tier = ET.SubElement(root, "TIER")
+            xwor_tier.set("TIER_ID", f"wor@{tier_id}")
+            xwor_tier.set("PARTICIPANT", tier_id)
+            xwor_tier.set("LINGUISTIC_TYPE_REF", "dependency")
+            xwor_tier.set("DEFAULT_LOCALE", "us")
+            xwor_tier.set("PARENT_REF", tier_id)
+
+            # we delete all previous xwor/wor tiers
+            if "wor" in tier_id or "xwor" in tier_id:
+                root.remove(tier)
+                continue
+
+            # we ignore anything that's a "@S*" tier
+            # because those are metadata
+            if "@" in tier_id:
+                continue
+
+            # For each annotation
+            for annotation in tier:
+                # get annotation
+                annotation = annotation[0]
+                # get ID
+                annot_id = annotation.attrib.get("ANNOTATION_ID", "0")
+
+                # append annotation line
+                # create two element
+                xwor_annot = ET.SubElement(xwor_tier, 'ANNOTATION')
+                xwor_annot_cont = ET.SubElement(xwor_annot, 'REF_ANNOTATION')
+
+                # adding annotation metadata
+                xwor_annot_cont.set("ANNOTATION_ID", f"xw{id_indx}")
+                xwor_annot_cont.set("ANNOTATION_REF", annot_id)
+
+                # and add content
+                xwor_word_cont = ET.SubElement(xwor_annot_cont, "ANNOTATION_VALUE")
+
+                # with the bulleted content
+                xwor_word_cont.text = terms_dict.get(annot_id)
+
+                # update index
+                id_indx += 1
+
+    # Remove the old time slot IDs
+    root[1].clear()
+
+    # For each of the resulting annotations, dump the time values
+    # Remember that time values dumped in milliseconds so we
+    # multiply the result by 1000 and round to int
+    for annotation, alignment in zip(annotations, alignments):
+        # Create the beginning time slot
+        element_start = ET.Element("TIME_SLOT")
+        # Set the time slot ID to the correct beginning
+        element_start.set("TIME_SLOT_ID", f"ts{annotation[0][0]}")
+
+        # Create the beginning time slot
+        element_end = ET.Element("TIME_SLOT")
+        # Set the time slot ID to the correct beginning
+        element_end.set("TIME_SLOT_ID", f"ts{annotation[0][1]}")
+
+        # if alignment is not None, set time value
+        if alignment:
+            # Set the time slot ID to the correct end
+            element_start.set("TIME_VALUE", f"{int(alignment[0]*1000)}")
+            # Set the time slot ID to the correct end
+            element_end.set("TIME_VALUE", f"{int(alignment[1]*1000)}")
+
+        # Appending the element
+        root[1].append(element_start)
+        root[1].append(element_end)
+
+    # Indent the tree
+    indent(root)
+    # And write tit to file
+    tree.write(output_path, encoding="unicode", xml_declaration=True)
+
+def elan2transcript(file_path):
+    """convert an eaf XML file to tiered transcripts
+
+    Attributes:
+        file_path (string): file path of the .eaf file
+
+    Returns:
+        a dictionary of shape {"transcript":transcript, "tiers":tiers},
+        with two arrays with transcript information.
+    """
+
+    # Load the file
+    parser = ET.XMLParser(encoding="utf-8")
+    tree = ET.parse(file_path, parser=parser)
+    root =  tree.getroot()
+    tiers = root[2:]
+
+    # create an array for annotations
+    annotations = []
+
+    # For each tier 
+    for tier in tiers:
+        # get the name of the tier
+        tier_id = tier.attrib.get("TIER_ID","")
+
+        # we ignore anything that's a "@S*" tier
+        # because those are metadata
+        if "@" in tier_id:
+            continue
+
+        # For each annotation
+        for annotation in tier:
+            # Get the text of the transcript
+            transcript = annotation[0][0].text
+            # and append and sort by the time slot references
+            try: 
+                timeslot_id = int(annotation[0].attrib.get("TIME_SLOT_REF1", "0")[2:])
+            except ValueError:
+                print(file_path)
+
+            # and append the metadata + transcript to the annotations
+            annotations.append((timeslot_id, tier_id, transcript))
+
+    # we will sort annotations by timeslot ref
+    annotations.sort(key=lambda x:x[0])
+
+    # we will seperate the tuples after sorting
+    _, tiers, transcript = zip(*annotations)
+
+    return {"transcript":transcript, "tiers":tiers}
+
+
+# chat2elan a whole path
+def chat2elan(directory):
+    """Convert a folder of CLAN .cha files to corresponding ELAN XMLs
+
+    Note: this function STRIPS EXISTING TIME CODES (though,
+          nondestructively)
+
+    files:
+        directory (string): the string directory in which .cha is in
+
+    Returns:
+        None
+    """
+   
+    # get all files in that directory
+    files = globase(directory, "*.cha")
+    # get the name of cleaned files
+    files_cleaned = [i.replace(".cha", ".new.cha") for i in files]
+
+    # then, clean out all bullets
+    for in_file, out_file in zip(files, files_cleaned):
+        # read the in file
+        with open(in_file, "r") as df:
+            in_file_content = df.read()
+
+        # perform cleaning and write
+        cleaned_content = re.sub("\x15.*?\x15", "", in_file_content)
+
+        # write
+        with open(out_file, "w") as df:
+            df.write(cleaned_content)
+            
+    # chat2elan it!
+    CMD = f"{os.path.join(CLAN_PATH, 'chat2elan')} +c +e.wav {' '.join(files_cleaned)} "
+
+    # run!
+    os.system(CMD)
+    # delete any error logs
+    for f in globase(directory, "*.err.cex"):
+        os.remove(f)
+    # delete intermediary
+    for f in globase(directory, "*.new.cha"):
+        os.remove(f)
+    # and rename the files
+    for f in globase(directory, "*.new.c2elan.eaf"):
+        os.rename(f, f.replace(".new.c2elan.eaf", ".eaf"))
+
+
+# chat2elan a whole path
+def elan2chat(directory):
+    """Convert a folder of CLAN .eaf files to corresponding CHATs
+
+    files:
+        directory (string): the string directory in which .elans are in
+
+    Returns:
+        None
+    """
+   
+    # get all files in that directory
+    files = globase(directory, "*.eaf")
+    # process each file
+    for fl in files:
+        # elan2chatit!
+        CMD = f"{os.path.join(CLAN_PATH, 'elan2chat +c ')} {fl} "
+        # run!
+        os.system(CMD)
+    # delete any error logs
+    for f in globase(directory, "*.err.cex"):
+        os.remove(f)
+    # and rename the files
+    for f in globase(directory, "*.elan.cha"):
+        os.rename(f, f.replace(".elan.cha", ".cha"))
+
+
